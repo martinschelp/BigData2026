4249dbfcc6e118f269906143d143b3dacb01cc9d
Guillermo Henrion <guillermo.henrion@gmail.com>
2026-03-18 11:10:45 -0300

    mlflow

--- clase-3/catalogo.tf
+++ clase-3/catalogo.tf
@@ -1,5 +1,5 @@
 resource "databricks_volume" "mlops_volume" {
-  name             = "mi_volume"
+  name             = "mi_volume2"
   catalog_name     = "mlops_dev"
   schema_name      = "default"   # o el schema que uses
   volume_type      = "MANAGED"  # o EXTERNAL
---
+++ clase-3/mlflow.tf
@@ -0,0 +1,3 @@
+resource "databricks_mlflow_experiment" "rf_breast_cancer" {
+  name = "/Users/guillermo.henrion@gmail.com/Itba_2026/"
+}
